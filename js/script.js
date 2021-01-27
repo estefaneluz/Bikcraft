bb9977c4640d67af5c9206fe06be9246e25f77a8
Estefane <estefanecortes@gmail.com>
2021-01-26 21:30:59 -0300

    Adicionando slide e animação nas páginas

---
+++ js/script.js
@@ -0,0 +1,16 @@
+if(window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    time: 5000
+  });
+
+  new SimpleSlide({
+    slide: "portfolio",
+    nav: true,
+    time: 5000
+  });
+}
+
+if(window.SimpleAnime) {
+  new SimpleAnime();
+}
